6c0f79a4e3177c47179735d10a4a18d295f0a92d
Sean Hoar <sean.hoar@gmail.com>
2021-08-28 19:52:00 -0400

    add react-router-dom and withRouter HOC to menu-item comp

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
-import React from 'react';
 import './App.css'
-
+import {Route, Switch} from 'react-router-dom';
 import HomePage from './pages/homepage/homepage.component'
+
+
+const HatPage = () => (
+  <div><h1>HATS PAGE</h1></div>
+)
 
 function App() {
   return (
     <div >
-      <HomePage />
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route path='/hats' component={HatPage} />
+      </Switch>
     </div>
   );
 }
